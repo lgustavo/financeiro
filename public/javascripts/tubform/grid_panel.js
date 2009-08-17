89d9f2ffa9323e85cd8653e72e093fba2796937c
Jefferson Jean Martins Girão <jefferson@jefferson-notebook.(none)>
2009-08-17 15:57:47 -0300

    Extensão dos componentes do EXTjs

---
+++ public/javascripts/tubform/grid_panel.js
@@ -0,0 +1,46 @@
+TUBFORM.GridPanel = Ext.extend(Ext.grid.GridPanel, {
+
+  constructor: function (config) {
+    Ext.apply(this, config);
+
+    Ext.apply(this, {
+      viewConfig: {
+        forceFit: true
+      },
+      sm: new Ext.grid.RowSelectionModel({singleSelect:true}),
+      height: 340,
+      width: 800,
+      iconCls: 'icon-grid',
+      autoShow: true
+    });
+
+    this.loadStore();
+    this.loadBar();
+
+    TUBFORM.GridPanel.superclass.constructor.call(this);
+
+  },
+
+  loadStore: function(){
+    this.store = new TUBFORM.Store({
+      entities: this.entities
+    });
+    this.store.load({params:{start: 0, limit: 5, page:1}});
+  },
+
+  loadBar: function() {
+    this.bbar = new TUBFORM.PagingToolbar({
+      store: this.store,
+      entity: this.entity,
+      entities: this.entities
+    });
+  },
+
+  onRender: function() {
+    TUBFORM.GridPanel.superclass.onRender.apply(this, arguments);
+  }
+
+});
+
+Ext.reg('tgrid', TUBFORM.GridPanel);
+
---
+++ public/javascripts/tubform/paging_toolbar.js
@@ -0,0 +1,38 @@
+TUBFORM.PagingToolbar = Ext.extend(Ext.PagingToolbar, {
+  constructor: function (config) {
+
+    // Aplica qualquer coisa especificada no objeto de configuração a esse objeto
+    Ext.apply(this, config);
+
+    Ext.apply(this, {
+      pageSize:5,
+      displayInfo: true,
+      items: ['-', {
+        text: 'Novo',
+        handler: function(){}
+      },{
+        text: 'Alterar',
+        handler: function(){}
+      }, {
+        text: 'Excluir',
+        handler: function(){}
+      }]
+    });
+
+    TUBFORM.PagingToolbar.superclass.constructor.call(this);
+  },
+
+  doLoad : function(start){
+    var o = {}, pn = this.paramNames;
+    o[pn.start] = start;
+    o[pn.limit] = this.pageSize;
+    o['page'] = Math.ceil((start+this.pageSize)/this.pageSize);
+    if(this.fireEvent('beforechange', this, o) !== false){
+      this.store.load({params:o});
+    }
+  }
+
+});
+
+Ext.reg('tpagingtoolbar', TUBFORM.PagingToolbar);
+
